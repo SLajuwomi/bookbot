2f342f956576374e57196d80c14b7dc96acf93e8
SLajuwomi <stephenlajuwomi@gmail.com>
2025-08-01 14:41:02 -0500

    formatting and some refactoring

--- main.py
+++ main.py
@@ -3,30 +3,37 @@ from stats import get_book_word_count
 from stats import get_book_char_count
 from stats import convert_char_dict_to_list
 
+
 def get_book_text(filepath):
     file_contents = ""
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+
 def main():
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {sys.argv[1]}...")
-    book_text = get_book_text(sys.argv[1])
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv(1)
+
+    book_text = get_book_text(book_path)
     book_word_count = get_book_word_count(book_text)
-    print("----------- Word Count ----------")
-    print(f"Found {book_word_count} total words")
     book_char_count = get_book_char_count(book_text)
     sorted_list_of_chars = convert_char_dict_to_list(book_char_count)
+    print_report()
+
+
+def print_report(book_path, book_word_count, sorted_list_of_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {book_word_count} total words")
     print("--------- Character Count -------")
     for item in sorted_list_of_chars:
         if item["char"].isalpha():
             print(f"{item["char"]}: {item["num"]}")
     print("============= END ===============")
 
-if len(sys.argv) != 2:
-    print("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
 
 main()
-
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ def get_book_word_count(book_text):
     book_text_list = book_text.split()
     return len(book_text_list)
 
+
 def get_book_char_count(book_text):
     char_dict = {}
     for char in book_text:
@@ -12,9 +13,11 @@ def get_book_char_count(book_text):
             char_dict[lower_char] += 1
     return char_dict
 
+
 def sort_on(items):
     return items["num"]
-      
+
+
 def convert_char_dict_to_list(char_dict):
     final_list = []
     for key in char_dict:
